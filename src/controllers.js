5a118ecb762916bc42feec2848f59b23ef0452be
GabR3924 <garc2004@gmail.com>
2024-02-12 21:33:03 -0400

    consumo

--- src/controllers.js
+++ src/controllers.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const PDFParser = require('pdf-parse');
+const Epub = require('epub-gen');
 
 const readPdf = async (req, res) => {
     const pdfPath = req.body.file;
@@ -15,9 +16,37 @@ const readPdf = async (req, res) => {
 
         console.log('Texto extraído del archivo PDF:', data.text);
 
-        // Aquí puedes procesar el texto como desees
-        // Por ejemplo, puedes enviarlo como respuesta JSON
-        res.status(200).json({ text: data.text });
+        // Crear un objeto de configuración para el archivo EPUB
+
+        const epubOptions = {
+            title: 'PDF Convertido a EPUB', // Título del libro EPUB
+            content: [{ title: 'Contenido', data: data.text }], // Contenido del libro EPUB
+            author: 'Autor del libro', // Autor del libro EPUB
+            output: pdfPath // Ruta de salida del archivo EPUB
+        };
+
+        console.log('opciones')
+
+        const generateEpub = new Promise((resolve, reject) => {
+            new Epub(epubOptions, error => {
+                if (error) {
+                    console.error('Error al generar el archivo EPUB:', error);
+                    reject(error);
+                } else {
+                    console.log('Archivo EPUB generado exitosamente');
+                    resolve();
+                }
+            });
+        });
+
+        console.log('esperando')
+        // Esperar a que se genere el archivo EPUB antes de responder
+        await generateEpub;
+
+        console.log('finalizado')
+        return res.status(200).json({ message: 'Archivo EPUB generado exitosamente' });
+
+        
     } catch (error) {
         console.error('Error al cargar o procesar el PDF:', error);
         res.status(500).json({ error: 'Error al cargar o procesar el PDF' });
