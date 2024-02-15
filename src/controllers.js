b780a616b6fe5b6f2bc8bacf5f71b392a73810ae
GabR3924 <garc2004@gmail.com>
2024-02-15 13:12:25 -0400

    casi

--- src/controllers.js
+++ src/controllers.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const PDFParser = require('pdf-parse');
-const Epub = require('epub-gen');
+const epub = require('epub-gen');
 
 const readPdf = async (req, res) => {
     const pdfPath = req.body.file;
@@ -27,25 +27,10 @@ const readPdf = async (req, res) => {
 
         console.log('opciones')
 
-        const generateEpub = new Promise((resolve, reject) => {
-            new Epub(epubOptions, error => {
-                if (error) {
-                    console.error('Error al generar el archivo EPUB:', error);
-                    reject(error);
-                } else {
-                    console.log('Archivo EPUB generado exitosamente');
-                    resolve();
-                }
-            });
-        });
-
-        console.log('esperando')
-        // Esperar a que se genere el archivo EPUB antes de responder
-        await generateEpub;
-
-        console.log('finalizado')
-        return res.status(200).json({ message: 'Archivo EPUB generado exitosamente' });
-
+        new epub(epubOptions).promise.then(() => {
+            console.log('Done');
+            res.download(epubOptions.output); // Devuelve el archivo EPUB como respuesta
+        })
         
     } catch (error) {
         console.error('Error al cargar o procesar el PDF:', error);
